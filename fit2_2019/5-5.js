c7e7dcd6d05f33c6b9cfe6747c63b0c4151eee23
Yuichi Furumoto <ao.42ao.42@gmail.com>
2019-10-21 15:40:06 +0900

    fifth
    15:40

--- fit2_2019/5-5.js
+++ fit2_2019/5-5.js
@@ -1,4 +1,18 @@
 function b1(){
-  var pAll = document.getElementsByTagName("p");
-  document.getElementById( "p" ).value = pAll.length ;
+  var a = document.getElementsByClassName("aaa");
+  alert(a.length);
+  var b = document.getElementsByClassName("bbb");
+  alert(b.length);
+  var c = document.getElementsByClassName("ccc");
+  alert(c.length);
+}
+
+function b2(){
+  var c = document.getElementsByClassName("ccc");
+  c.innerHTML = "みんな違ってみんないい。ただしてめえはだめ。"
+}
+
+function b3() {
+  var ab = document.getElementsByClassName("aaa bbb");
+  alert(ab.length);
 }
