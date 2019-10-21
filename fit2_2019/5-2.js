57034674315e128797f2b37e5bd53c5506834c08
Yuichi Furumoto <ao.42ao.42@gmail.com>
2019-10-21 14:43:59 +0900

    thaad
    14:43

--- fit2_2019/5-2.js
+++ fit2_2019/5-2.js
@@ -6,15 +6,14 @@ function hai(){
   var n5 = prompt("前回とは違う名前を入力してください");
   var n6 = prompt("前回とは違う名前を入力してください");
 
-  // 元となる配列
   const name=[n1,n2,n3,n4,n5,n6];
-  // いくつずつに分けるか
+  name.sort();
   const divideLength = 3;
 
   for(let i = 0; i < name.length; i += divideLength){
-   　// 指定した個数ずつに分割する
+   　// 指定した個数ずつに分割
    　let splitname = name.slice(i, i + divideLength);
-   　// カンマで接続し、文字列にする
+   　// カンマで接続、文字列に
    　let numStr = splitname.join();
    　alert(numStr);
   }
