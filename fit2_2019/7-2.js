a1a46af34b40a13dd8e6d591f4da17e60802bbbf
Yuichi Furumoto <ao.42ao.42@gmail.com>
2019-11-11 15:34:51 +0900

    前半ーN数描画まで

---
+++ fit2_2019/7-2.js
@@ -0,0 +1,29 @@
+function Dr() {
+
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+
+  var x = 100;  // 中心点のX座標
+  var y = 100;  // 中心点のY座標
+  var r = 80;   // 半径
+
+  ctx.lineWidth = 3.0;
+  ctx.strokeStyle = 'black';
+  ctx.fillStyle = 'lightgrey';
+
+  var nsu = document.getElementById("Nsu").value;
+
+  ctx.beginPath();
+  ctx.moveTo(x + Math.sin(0 * 2 * Math.PI / 6) * r, y - Math.cos(0 * 2 * Math.PI / 6) * r);
+
+  var z = 0
+
+  for(z = 0; z <= nsu; z++){
+  ctx.lineTo(x + Math.sin(z * 2 * Math.PI / nsu) * r, y - Math.cos(z * 2 * Math.PI / nsu) * r);
+  }
+
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+
+}
