f7d7b916a5444431aa0aa3bd0f8b2225be611c65
Yuichi Furumoto <ao.42ao.42@gmail.com>
2019-11-12 11:14:17 +0900

    7-6実装

--- fit2_2019/7-6.js
+++ fit2_2019/7-6.js
@@ -5,6 +5,10 @@ var x, y;
 for (y=0; y<200; y+=20) {
   for (x=0; x<200; x+=20) {
     if ((x+y)%80 == 0) {
+      ctx.fillStyle = 'Black';
+      ctx.fillRect(x, y, 20, 20);
+    }else if ((x+y)%40 == 0) {
+      ctx.fillStyle = 'lightgrey';
       ctx.fillRect(x, y, 20, 20);
     }
   }
