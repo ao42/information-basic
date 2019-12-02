68e89f9ddc3410cc6fcdb078e9a918e5a36b9f77
Yuichi Furumoto <40926437+ao42@users.noreply.github.com>
2019-12-02 17:26:19 +0900

    console.log追加

--- fit2_2019/9-2.js
+++ fit2_2019/9-2.js
@@ -1,6 +1,7 @@
 function light_print(event) {
     var elm = document.getElementById('lighttext');
     elm.value = event.value;
+    console.log(event.value)
 }
 
 window.addEventListener('devicelight', light_print);
