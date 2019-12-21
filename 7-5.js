6c1c2af509ac7695417254b2d85fa0901378242f
Yuichi Furumoto <40926437+ao42@users.noreply.github.com>
2019-12-21 16:54:15 +0900

    Mi

--- 7-5.js
+++ 7-5.js
@@ -2,10 +2,8 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
 var x, y;
-for (y=0; y<100; y+=10) {
-  for (x=0; x<100; x+=10) {
-    if ((x+y)%80 == 0) {
+for (y=50; y>100; y-=10) {
+  for (x=40; x>100; x-=10) {
       ctx.fillRect(x, y, 10, 10);
-    }
   }
 }
